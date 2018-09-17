3f5995858fbd61962b098de1f6d06048e26b1449
Ulises <ulis2_rom@hotmail.com>
2018-09-17 10:34:23 -0500

    creacion del servicio de cursos

--- school-mobiik/school-mobiik/ClientApp/src/app/components/cursos/cursos.component.ts
+++ school-mobiik/school-mobiik/ClientApp/src/app/components/cursos/cursos.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cursos',
-  templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  templateUrl: './cursos.component.html'
 })
 export class CursosComponent implements OnInit {
 
