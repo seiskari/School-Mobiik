b4796ea6df82e9f7016651e65f37a9197fbd8df1
Oscar Fabian <azulyoro.unam@hotmail.com>
2018-09-18 09:47:17 -0500

    Agrego CurseMainDto

---
+++ school-mobiik/SchoolMobiik.DTOs/CurseMainDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SchoolMobiik.Entities;
+
+namespace SchoolMobiik.DTOs
+{
+    class CurseMainDto
+    {               
+        public int SchoolId { get; set; }      
+        public Teacher Teacher { get; set; }
+        public Schedule Schedule { get; set; }
+        public Signatures Signatures { get; set; }
+    }
+}
