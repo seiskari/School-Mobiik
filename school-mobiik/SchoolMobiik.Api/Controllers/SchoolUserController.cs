7f466f2796cc099fc986ae882fc08f7813c53398
ugom93 <ugom93@hotmail.com>
2018-09-18 11:39:25 -0500

    Se creó Controller, B.L. y DA. de UserSchool

---
+++ school-mobiik/SchoolMobiik.Api/Controllers/SchoolUserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SchoolMobiik.DTOs;
+using SchoolMobiik.BussinessLayer;
+using System.Net;
+using System.Web.Http.Description;
+using System.Web.Http;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using System.Net.Http;
+
+namespace SchoolMobiik.Api.Controllers
+{
+    [Route("api/SchoolUser")]
+    public class SchoolUserController : ApiController
+    {
+        ProccessUserSchool proccessUserSchool;
+
+        public SchoolUserController()
+        {
+            proccessUserSchool = new ProccessUserSchool();
+        }
+
+        [HttpGet, Route("SchoolUser/{userName}/{password}")]
+        [ResponseType(typeof(SchoolUserDTO))]
+        public HttpResponseMessage GetSchoolUser (string userName, string password)
+        {
+            SchoolUserDTO schoolUserDTO = proccessUserSchool.GetUserSchool(userName, password);
+            if (schoolUserDTO != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+        }
+
+    }
+}
---
+++ school-mobiik/SchoolMobiik.DTOs/SchoolUserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolMobiik.DTOs
+{
+    public class SchoolUserDTO
+    {
+        public int PersonId { get; set; }
+        public int RolId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
