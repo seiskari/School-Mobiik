21cb4343acc950f59b15bbe7b3e11ec265333eeb
Iovany Giron Moreno <iovany.giron@mobiik.com>
2018-09-20 11:27:15 -0500

    commit para seguirr trabajando en  UserRegister

---
+++ school-mobiik/SchoolMobiik.DTOs/UserRegisterDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolMobiik.DTOs
+{
+    public class UserRegisterDTO
+    {
+        public DateTime Birthday { get; set; }
+        public string CURP { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SecondLastName { get; set; }
+        public string Street { get; set; }
+        public string ExtNum { get; set; }
+        public string IntNum { get; set; }
+        public int Neighborhood { get; set; }
+        public string UserName {get;set;}
+        public string Password { get; set; }
+    }
+}
---
+++ school-mobiik/SchoolMobiik.DataAccess/SQLDataAccessUserRegister.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SchoolMobiik.Entity;
+using System.Linq;
+
+namespace SchoolMobiik.DataAccess
+{
+    public class SQLDataAccessUserRegister : IDisposable
+    {
+
+        public SQLDataAccessUserRegister()
+        {
+        }
+
+        public Person GetPerson()
+        {
+            using (var context = new SemillerosDBContext())
+            {
+
+            }
+        }
+
+        public SchoolUser GetSchoolUser()
+        {
+            using (var context = new SemillerosDBContext())
+            {
+
+            }
+        }
+
+        public Address GetAddress()
+        {
+            using (var context = new SemillerosDBContext())
+            {
+
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
